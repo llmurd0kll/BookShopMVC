cbfe5ba90c85f10961cf4914583a9ce19460ea70
Viktor <vitalineage22@gmail.com>
2024-05-13 15:05:04 +0300

    Add project files.

---
+++ BookShopMVCUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookShopMVCUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
